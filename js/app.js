cada0e8e882a29c98000164667e871d1df67e385
Emi Ko <emi8emi@gmail.com>
2019-05-21 16:03:49 -0700

    Create intro modal

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
-const adviceSlip_div = document.getElementById('advice-slip')
-const advice = document.getElementById('advice')
-const clear = () => advice.innerHTML = ""
-const advise = () => {fetch('https://api.adviceslip.com/advice').then(response => response.json()).then(data =>  advice.append(`${data.slip.advice}`))}
+const adviceSlip_div = document.getElementById('advice-slip');
+const advice = document.getElementById('advice');
+const close = document.getElementById('close');
+const intro = document.getElementById('intro');
+const clear = () => advice.innerHTML = "";
+const advise = () => {fetch('https://api.adviceslip.com/advice').then(response => response.json()).then(data =>  advice.append(`${data.slip.advice}`))};
+
+const introTimeOut = () => {
+  setTimeout(function(){$('#intro').hide()}, 5000
+)}
 
 const main = () => {
-adviceSlip_div.addEventListener('click', function() {clear(); advise(); $('#advice').show().fadeOut(9000)})
-}
+adviceSlip_div.addEventListener('click', function() {clear(); advise(); $('#advice').show().fadeOut(9000)});
+
+close.addEventListener('click', function() {
+  $('#intro').hide()
+})
+};
 
 main()
+introTimeOut()
