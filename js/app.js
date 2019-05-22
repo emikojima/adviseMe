765ca928ebf36ce996f50b841c901d1b67b2cc54
Emi Ko <emi8emi@gmail.com>
2019-05-21 17:33:04 -0700

    Fix setTimeout feature for intro to be 9 sec instead of 5

--- js/app.js
+++ js/app.js
@@ -2,20 +2,19 @@ const adviceSlip_div = document.getElementById('advice-slip');
 const advice = document.getElementById('advice');
 const close = document.getElementById('close');
 const intro = document.getElementById('intro');
+
 const clear = () => advice.innerHTML = "";
 const advise = () => {fetch('https://api.adviceslip.com/advice').then(response => response.json()).then(data =>  advice.append(`${data.slip.advice}`))};
 
 const introTimeOut = () => {
-  setTimeout(function(){$('#intro').hide()}, 5000
+  setTimeout(function(){$('#intro').hide()}, 9000
 )}
 
 const main = () => {
 adviceSlip_div.addEventListener('click', function() {clear(); advise();$('#advice').show().fadeOut(9000);$('#advice-slip').css({"pointer-events": "none"}); setTimeout(function(){$('#advice-slip').css("pointer-events", "auto")}, 8000)});
 
-
 close.addEventListener('click', function() {
-  $('#intro').hide()
-})
+  $('#intro').hide()})
 };
 
 main()
