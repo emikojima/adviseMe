74b1fdbd45b5ed2d92ff61561aa0592fcf9f5860
Emi Ko <emi8emi@gmail.com>
2019-05-28 22:19:18 -0700

    Refactor app.ja Clean up css

--- js/app.js
+++ js/app.js
@@ -4,14 +4,29 @@ const close = document.getElementById('close');
 const intro = document.getElementById('intro');
 
 const clear = () => advice.innerHTML = "";
+const change = () => adviceSlip_div.innerHTML = "";
 const advise = () => {fetch('https://api.adviceslip.com/advice').then(response => response.json()).then(data =>  advice.append(`${data.slip.advice}`))};
+const heart = $('<p>For you <br> with &#x2665</p>')
 
 const introTimeOut = () => {
-  setTimeout(function(){$('#intro').hide()}, 9000
+  setTimeout(function(){$('#intro').hide()}, 10000
 )}
 
 const main = () => {
-adviceSlip_div.addEventListener('click', function() {clear(); advise();$('#advice').show().fadeOut(9000);$('#advice-slip').css({"pointer-events": "none"}); setTimeout(function(){$('#advice-slip').css("pointer-events", "auto")}, 8000)});
+  adviceSlip_div.addEventListener('click', function() {
+    clear();
+    advise();
+    $('#advice').show().fadeOut(9000);
+    $('#advice-slip').css({"pointer-events": "none"});
+    change();
+    $('#advice-slip').append(heart[0]);
+    $('#advice-slip').fadeOut(9000);
+
+    setTimeout(function(){
+      $('#advice-slip').css({"pointer-events": "auto", "border-color": "pink", "box-shadow": "0 10px 10px -10px pink"})
+      change();
+      $('#advice-slip').append("GET ADVICE").show()}, 9100);
+})
 
 close.addEventListener('click', function() {
   $('#intro').hide()})
